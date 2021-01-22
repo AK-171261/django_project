07f778ad975c0af5c6d43d44440258acaf6cf182
Anand Kumar <singhanandkr94@gmail.com>
2021-01-22 11:37:33 +0530

    Login/Logout

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 
@@ -14,3 +15,8 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {"form": form})
+
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
