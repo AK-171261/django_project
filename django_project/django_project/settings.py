103999e7961de1550513037434e62708003777ea
Anand Kumar <singhanandkr94@gmail.com>
2021-01-26 17:15:25 +0530

    user profile and picture/update user profile

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -133,7 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
