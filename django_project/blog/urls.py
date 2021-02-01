86df98e8a44e7205c9b646028a2241fb29d2d8b6
Anand Kumar <singhanandkr94@gmail.com>
2021-02-01 06:51:26 +0530

    pagination/post by particular user/password reset functionality

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from django.urls import path
 
 urlpatterns = [
     # path('', views.home, name='blog-home'),  #function based views
     path('', PostListView.as_view(), name='blog-home'),  # class based views
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),  # class based views
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
